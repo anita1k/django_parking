5c10b7d802de8e6cb4fe5a594fd7f5b533ce0056
anita1k <99723523+anita1k@users.noreply.github.com>
2024-06-15 22:29:22 +0300

    Update settings.py - DATABASE_URL

--- parkingmanager/settings.py
+++ parkingmanager/settings.py
@@ -137,17 +137,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASE_URL = os.environ.get('DATABASE_URL', 'postgres://admin:admin@127.0.0.1:5432/parking_db')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# The URL to use when referring to static files (where they will be served from)
-STATIC_URL = '/static/'
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
